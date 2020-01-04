19d26647f7eb4a2824509252c1404f1412f0736f
YukiTeito <xdi.grand.you.winner@gmail.com>
2020-01-05 01:41:04 +0900

    add score and score ui refresh Mob2

---
+++ Assets/ScoreCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCount : MonoBehaviour
+{
+    public static int score;
+    
+    public static void scoreCount() {
+        score++;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
